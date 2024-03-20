30be6804479379ac76d5cbcbf4a7b9eeb6564441
Egor <eg.komarov14@gmail.com>
2024-03-20 14:51:13 +0100

    +logging

--- src/main/java/com/techonlinestore/controller/BrandController.java
+++ src/main/java/com/techonlinestore/controller/BrandController.java
@@ -5,6 +5,7 @@ import com.techonlinestore.mapper.BrandMapper;
 import com.techonlinestore.service.BrandService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/brand")
 public class BrandController {
@@ -25,43 +27,55 @@ public class BrandController {
 
 	@GetMapping("/{brandId}")
 	public BrandDto getBrandById(@PathVariable("brandId") int brandId) {
+		log.info("Request to get brand with ID: {}", brandId);
 		BrandDto brandDto = brandService.getBrandById(brandId);
 
 		if (brandDto != null) {
+			log.info("Brand with ID: {} found", brandId);
 			return brandDto;
 		} else {
+			log.error("Brand with ID: {} not found", brandId);
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Brand not found");
 		}
 	}
 
 	@GetMapping("/all")
 	public List<BrandDto> getAllBrands() {
+		log.info("Request to get all brands");
 		return brandService.getAllBrands();
 	}
 
 	@PostMapping
 	public ResponseEntity<Object> createBrand(@Valid @RequestBody BrandDto brandDto) {
-		var createdBrand = brandService.createBrand(brandDto);
+		log.info("Request to create a new brand");
+		BrandDto createdBrand = brandService.createBrand(brandDto);
 
-		if (createdBrand == null) {
-			return ResponseEntity.badRequest().body("Unable to create brand. Check your request.");
+		if (createdBrand != null){
+			log.info("Brand created successfully with ID: {}", brandMapper.toLastBrand(createdBrand).getBrandId());
+			URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{brandId}")
+				.buildAndExpand(brandMapper.toLastBrand(createdBrand).getBrandId()).toUri();
+			return ResponseEntity.created(location).build();
+		} else {
+			log.error("Failed to create brand");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to create brand");
 		}
 
-		URI location = ServletUriComponentsBuilder
-			.fromCurrentRequest()
-			.path("/{brandId}")
-			.buildAndExpand(brandMapper.toLastBrand(createdBrand).getBrandId()).toUri();
 
-		return ResponseEntity.created(location).build();
+
 	}
 
 	@PutMapping("/{brandId}")
 	public BrandDto updateBrand(@PathVariable("brandId") int brandId,@Valid @RequestBody BrandDto brandDto) {
+		log.info("Request to update brand with ID: {}", brandId);
 		BrandDto updatedBrand = brandService.updateBrand(brandId,brandDto);
 
 		if (updatedBrand != null) {
+			log.info("Product with ID: {} updated successfully", brandId);
 			return brandDto;
 		} else {
+			log.error("Failed to update brand with ID: {}", brandId);
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Brand not found");
 		}
 	}
@@ -69,6 +83,8 @@ public class BrandController {
 	@DeleteMapping("/{brandId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteBrand(@PathVariable("brandId") int brandId) {
+		log.info("Request to delete brand with ID: {}", brandId);
 		brandService.deleteBrand(brandId);
+		log.info("Brand with ID: {} deleted successfully", brandId);
 	}
 }
--- src/main/java/com/techonlinestore/controller/ProductController.java
+++ src/main/java/com/techonlinestore/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.techonlinestore.mapper.ProductMapper;
 import com.techonlinestore.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/product")
 public class ProductController {
@@ -24,42 +26,51 @@ public class ProductController {
 
 	@GetMapping("/{productId}")
 	public ProductDto getProductById(@PathVariable("productId") int productId){
+		log.info("Request to get product with ID: {}", productId);
 		ProductDto productDto = productService.getProductById(productId);
 
 		if (productDto != null) {
+			log.info("Product with ID: {} found", productId);
 			return productDto;
 		} else {
+			log.error("Product with ID: {} not found", productId);
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
 		}
 	}
 
 	@GetMapping("/all")
 	public List<ProductDto> getAllProducts() {
+		log.info("Request to get all products");
 		return productService.getAllProducts();
 	}
 
 	@PostMapping
 	public ResponseEntity<Object> createProduct(@Valid @RequestBody ProductDto productDto) {
-		var createdProduct = productService.createProduct(productDto);
-
-		if (createdProduct == null){
-			return ResponseEntity.badRequest().body("Unable to create product. Check your request.");
+		log.info("Request to create a new product");
+		ProductDto createdProduct = productService.createProduct(productDto);
+		if (createdProduct != null){
+			log.info("Product created successfully with ID: {}", productMapper.toLastProduct(createdProduct).getProductId());
+			URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{productId}")
+				.buildAndExpand(productMapper.toLastProduct(createdProduct).getProductId()).toUri();
+			return ResponseEntity.created(location).build();
+		} else {
+			log.error("Failed to create product");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to create product");
 		}
-		URI location = ServletUriComponentsBuilder
-			.fromCurrentRequest()
-			.path("/{productId}")
-			.buildAndExpand(productMapper.toLastProduct(createdProduct).getProductId()).toUri();
-
-		return ResponseEntity.created(location).build();
 	}
 
 	@PutMapping("/{productId}")
 	public ProductDto updateProduct(@PathVariable("productId") int productId, @Valid @RequestBody ProductDto productDto) {
+		log.info("Request to update product with ID: {}", productId);
 		ProductDto updatedProduct = productService.updateProduct(productId, productDto);
 
 		if (updatedProduct != null) {
+			log.info("Product with ID: {} updated successfully", productId);
 			return updatedProduct;
 		} else {
+			log.error("Failed to update product with ID: {}", productId);
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
 		}
 	}
@@ -67,6 +78,8 @@ public class ProductController {
 	@DeleteMapping("/{productId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteProduct(@PathVariable("productId") int productId) {
+		log.info("Request to delete product with ID: {}", productId);
 		productService.deleteProduct(productId);
+		log.info("Product with ID: {} deleted successfully", productId);
 	}
 }
